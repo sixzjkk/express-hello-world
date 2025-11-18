0818f520ff4fb8f8f29af23b97784db9138e46f4
sixzjkk <jenifer.porfirio@estudante.ifms.edu.br>
2025-11-18 13:38:14 -0400

    Add GET /tico

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get('/meunome', (req, res) => {
     res.send('Meu nome é Jenifer Gabriely Moreira Porfirio')
 })
 
+app.get('/tico', (req, res) => {
+    res.send('teco')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
