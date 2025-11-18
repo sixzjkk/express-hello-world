76a598d1f9ca902fb98874b0676ae18e090d0092
sixzjkk <jenifer.porfirio@estudante.ifms.edu.br>
2025-11-18 13:42:13 -0400

    Add GET /pokemons

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.get('/tico', (req, res) => {
     res.send('teco')
 })
 
+app.get('/pokemons', (req, res) => {
+  const pokemons = [ "Caterpie", "Pidgeotto", "Bulbasaur", "Charmander", "Squirtle", "Krabby", "Muk", "Tauros", "Lapras", "Snorlax"];
+    res.json(pokemons)
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
