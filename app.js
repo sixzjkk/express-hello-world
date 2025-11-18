c88b5628ded708c048d7850bc3ac14922a91c08c
sixzjkk <jenifer.porfirio@estudante.ifms.edu.br>
2025-11-18 13:37:24 -0400

    Add GET /meunome

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get('/meunome', (req, res) => {
+    res.send('Meu nome é Jenifer Gabriely Moreira Porfirio')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
